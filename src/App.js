69fa2c62470b81b7b269b822f4666ab4164ac77b
Andrew Paramoshkin <paramoshkin.andrew@gmail.com>
2019-03-06 01:25:11 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Layout } from 'antd';
+import 'antd/dist/antd.css';
 import './App.css';
+
+const { Content, Sider } = Layout;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Layout className="layout" style={{ height: "100vh" }} tagName="main">
+        <Layout tagName="section">
+          <Sider width={300} style={{ background: '#fff', padding: '24px' }}>
+            Form
+          </Sider>
+          <Layout style={{ padding: '24px' }} tagName="section">
+            <Content tagName="main">
+              Content
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
     );
   }
 }
